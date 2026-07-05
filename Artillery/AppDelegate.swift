6170c0f1db7509bd5b4d1ab7790c737ada69063c
David Burgstrom <david.burgstrom@outlook.com>
2026-07-04 21:35:44 -0400

    Initial Commit

---
+++ Artillery/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Artillery
+//
+//  Created by David Burgstrom on 7/4/26.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ ArtilleryTests/ArtilleryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtilleryTests.swift
+//  ArtilleryTests
+//
+//  Created by David Burgstrom on 7/4/26.
+//
+
+import Testing
+@testable import Artillery
+
+struct ArtilleryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
